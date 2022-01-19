44a9fa0eb800d34eeb192992bf8173b220c4e6e5
quici9 <buicuong91196@gmail.com>
2022-01-19 22:08:16 +0700

    Generate snake and animate it.

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+from turtle import Screen
+from snake import Snake
+
+screen = Screen()
+screen.title("My Snake Game")
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+screen.exitonclick()
